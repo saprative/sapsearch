e06742a8cdd74160d546222ddba51d0ad466e7ce
saprative <saprative@gmail.com>
2017-04-02 08:53:18 +0530

    Concurrent calling implemented

--- routes/handler.go
+++ routes/handler.go
@@ -12,8 +12,8 @@ import (
 func Handler(w http.ResponseWriter, r *http.Request) {
 
 	q := r.URL.Query().Get("q")
-	//a := make(chan interface{})
-	//b := make(chan interface{})
+	google_ch := make(chan string)
+	duck_ch := make(chan string)
 
 	Google := new(models.GoogleModel)
 	Duckduckgo := new(models.DuckDuckGoModel)
@@ -22,22 +22,20 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	google_url := "https://www.googleapis.com/customsearch/v1?key=AIzaSyDvhG9Yg_OfscGLx9nKLZY7iR5r-yeN_xg&cx=017576662512468239146:omuauf_lfve&q=" + q
 	duckduckgo_url := "http://api.duckduckgo.com/?q=" + q + "&format=json"
 
-	makerequest.MakeRequest(google_url, Google)
+	go func() {
+		makerequest.MakeRequest(google_url, Google)
+		google_ch <- Google.Items[0].Snippet
+	}()
 
-	//if err_google != nil {
-	//log.Printf(err_google.Error())
-	//}
-
-	makerequest.MakeRequest(duckduckgo_url, Duckduckgo)
-
-	//if err_duckduckgo != nil {
-	//log.Printf(err_duckduckgo.Error())
-	//}
+	go func() {
+		makerequest.MakeRequest(duckduckgo_url, Duckduckgo)
+		duck_ch <- Duckduckgo.RelatedTopics[0].Text
+	}()
 
 	MySearch.Results.Google.URL = google_url
-	MySearch.Results.Google.Text = Google.Items[0].Snippet
+	MySearch.Results.Google.Text = <-google_ch
 	MySearch.Results.Duckduckgo.URL = duckduckgo_url
-	MySearch.Results.Duckduckgo.Text = Duckduckgo.RelatedTopics[0].Text
+	MySearch.Results.Duckduckgo.Text = <-duck_ch
 
 	out, err := json.Marshal(MySearch)
 	if err != nil {
