365326faa5f1cfbe706c91ca2f9a4a9891d91c38
saprative <saprative@gmail.com>
2017-04-02 15:26:50 +0530

    twitter call implemented

--- models/mysearch.go
+++ models/mysearch.go
@@ -7,10 +7,10 @@ type MySearchModel struct {
 			URL  string `json:"url"`
 			Text string `json:"text"`
 		} `json:"google"`
-		//Twitter struct {
-		//URL  string `json:"url"`
-		//Text string `json:"text"`
-		//} `json:"twitter"`
+		Twitter struct {
+			URL  string `json:"url"`
+			Text string `json:"text"`
+		} `json:"twitter"`
 		Duckduckgo struct {
 			URL  string `json:"url"`
 			Text string `json:"text"`
--- routes/handler.go
+++ routes/handler.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/ChimeraCoder/anaconda"
 	"github.com/saprative/sapsearch/models"
 	"github.com/saprative/sapsearch/utils"
 	"log"
@@ -16,6 +17,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	google_ch := make(chan string)
 	duck_ch := make(chan string)
+	twitter_ch := make(chan string)
 
 	Google := new(models.GoogleModel)
 	Duckduckgo := new(models.DuckDuckGoModel)
@@ -25,6 +27,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	google_url := "https://www.googleapis.com/customsearch/v1?key=AIzaSyDvhG9Yg_OfscGLx9nKLZY7iR5r-yeN_xg&cx=017576662512468239146:omuauf_lfve&q=" + q
 	duckduckgo_url := "http://api.duckduckgo.com/?q=" + q + "&format=json"
+	twitter_url := "https://api.twitter.com/1.1/search/tweets.json?q=" + q
 
 	go func() {
 		utils.MakeRequest(google_url, Google)
@@ -36,10 +39,22 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		duck_ch <- Duckduckgo.RelatedTopics[0].Text
 	}()
 
+	go func() {
+		anaconda.SetConsumerKey("mSTKWCzHDUIXTy5hpeQxFcz3X")
+		anaconda.SetConsumerSecret("xWAZZRFn9H3AG3iQFdYzSgA1bq4KMhqhSalHlAi1BvcsFY6kBb")
+		api := anaconda.NewTwitterApi("294750312-ECGKh0GlTU8QarIlhaMi0xLDFsHRlQzNafDiQxxF", "78ZNZhXSKb6QdmPdxSfiICOjZWNrWscTxEPvMgxfSYQ5k")
+		searchResult, _ := api.GetSearch(q, nil)
+		tweet := searchResult.Statuses
+
+		twitter_ch <- tweet[0].Text
+	}()
+
 	MySearch.Results.Google.URL = google_url
 	MySearch.Results.Google.Text = <-google_ch
 	MySearch.Results.Duckduckgo.URL = duckduckgo_url
 	MySearch.Results.Duckduckgo.Text = <-duck_ch
+	MySearch.Results.Twitter.URL = twitter_url
+	MySearch.Results.Twitter.Text = <-twitter_ch
 
 	out, err := json.Marshal(MySearch)
 	if err != nil {
